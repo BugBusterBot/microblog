7c71a9308411585ac0f5812974c6e4604ca56973
BugBusterBot <johnrambo2236@gmail.com>
2024-11-07 16:37:17 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -182,8 +182,8 @@ def reset_password_request():
         return redirect(url_for("login"))
     return render_template("reset_password_request.html", title ="Reset Password", form=form)
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
+@app.route('/reset_password_email/<token>', methods=['GET', 'POST'])
+def reset_password_email(token):
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     user = User.verify_reset_password_token(token)
@@ -196,6 +196,18 @@ def reset_password(token):
         flash('Your password has been reset.')
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+@login_required
+def reset_password():
+    name = request.args.get("name")
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        current_user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form, name=name)
 
 @app.route("/remove_post/<id>", methods=["POST"])
 @login_required
