4c4fa3827759eb424dd4ec953f3b66e7424c8b9a
adnquang29 <adnquang29@users.noreply.github.com>
2025-11-05 14:09:49 +0700

    Routes.py: Điều hướng chức năng cho web forms.py là form điền trên web models.py là kiến trúc database
    File đã sửa: Routes.py, Base.html, User.html
    File đã tạo: Home.html, Datasources.html, Checklog.html
    Folder đã tạo: stylesheet

--- app/routes.py
+++ app/routes.py
@@ -20,20 +20,21 @@ def before_request():
 @app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash("Your post is now live")
-        return redirect(url_for("index"))
-    page = request.args.get("page", 1, type=int)
-    posts = db.paginate(current_user.following_posts(), page=page, 
-                        per_page=app.config["POSTS_PER_PAGE"], error_out=False)
-    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
-    return render_template("index.html", title="Home Page", form=form, posts=posts.items,
-                           next_url = next_url, prev_url = prev_url)
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         post = Post(body=form.post.data, author=current_user)
+#         db.session.add(post)
+#         db.session.commit()
+#         flash("Your post is now live")
+#         return redirect(url_for("index"))
+#     page = request.args.get("page", 1, type=int)
+#     posts = db.paginate(current_user.following_posts(), page=page, 
+#                         per_page=app.config["POSTS_PER_PAGE"], error_out=False)
+#     next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+#     prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+#     return render_template("index.html", title="Home Page", form=form, posts=posts.items,
+#                            next_url = next_url, prev_url = prev_url)
+    return render_template("home.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -231,3 +232,21 @@ def edit_post(id):
     elif request.method == "GET":
         form.body.data = post.body
     return render_template("edit_post.html", title="Edit Post", form=form)
+
+@app.route("/checklog", methods=["GET", "POST"])
+@login_required
+def checklog():
+
+    return render_template("checklog.html", title="Check Log")
+
+@app.route("/datasources", methods=["GET", "POST"])
+@login_required
+def datasources():
+
+    return render_template("datasources.html", title="Data Sources")
+
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
+
+    return render_template("home.html", title="Home")
